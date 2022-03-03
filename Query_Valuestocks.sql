8af0a8111b6d79ef448cade43feace6e61956117
Sauravupd <upd6196@gmail.com>
2022-03-03 21:26:48 +0545

    finished query stock values

---
+++ Query_Valuestocks.sql
@@ -0,0 +1,6 @@
+with t1 as (select company ,CAST(RIGHT(CAST(fiscal_year as VARCHAR), 4)AS Integer)-(row_number() over (order by company)) as difference,
+(row_number() over (order by company))as RN FROM dividend 
+),
+t2 as (select *,count(*) over (partition by difference)as number_of_partition from t1)
+
+select distinct(company) from t2 where number_of_partition >=3
